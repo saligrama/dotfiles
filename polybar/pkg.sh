b7046d87dcde554aad8692ea200d37dcf6df2a4a
Aditya Saligrama <saligrama427@gmail.com>
2017-09-28 18:09:11 -0400

    chalk update

--- polybar/pkg.sh
+++ polybar/pkg.sh
@@ -3,7 +3,9 @@ pac=$(checkupdates | wc -l)
 aur=$(cower -u | wc -l)
 
 check=$((pac + aur))
-if [[ "$check" != "0" ]]
-then
-    echo "%{F#eceff1}$pac%{F-} %{F#ff9800}%{F-} %{F#eceff1}$aur%{F-}"
+if [[ "$check" != "0" ]]; then
+    echo "$pac %{F#d33682}%{F-} $aur"
+else
+    echo ""
+    exit
 fi
